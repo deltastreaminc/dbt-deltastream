113ec0c057e31b6b68212297ba16cb13382ef46f
Christophe Oudar <kayrnt@gmail.com>
2025-05-19 10:31:19 -1000

    Databricks + Deltastream

--- examples/databricks_example/models/pageviews_zipcode_per_minute.sql
+++ examples/databricks_example/models/pageviews_zipcode_per_minute.sql
@@ -1,38 +1,13 @@
 {{
-    config(
-        materialized='table'
-    )
+  config(
+    materialized='materialized_view'
+  )
 }}
-
-WITH hopping_windows AS (
-    SELECT
-        zipcode,
-        -- Generate window_start for each event, for both the current and previous 30s interval
-        explode(
-            array(
-                date_trunc('minute', viewtime) + INTERVAL 0 seconds,
-                date_trunc('minute', viewtime) + INTERVAL 30 seconds
-            )
-        ) AS window_start,
-        viewtime
-    FROM {{ ref('csas_zipcode_pageviews') }}
-),
-windowed_data AS (
-    SELECT
-        window_start,
-        window_start + INTERVAL 1 minute AS window_end,
-        zipcode,
-        COUNT(*) AS pageviews
-    FROM hopping_windows
-    WHERE viewtime >= window_start
-      AND viewtime < window_start + INTERVAL 1 minute
-    GROUP BY window_start, window_start + INTERVAL 1 minute, zipcode
-)
-
-SELECT
-    window_start,
-    window_end,
-    zipcode,
-    pageviews
-FROM windowed_data
-ORDER BY window_start, zipcode
+SELECT 
+  window_start, 
+  window_end,
+  zipcode, 
+  COUNT(*) AS pageviews
+FROM HOP ({{ ref('csas_zipcode_pageviews') }}, SIZE 1 minute, ADVANCE BY 30 second)
+WITH ('timestamp'='viewtime')
+GROUP BY window_start, window_end, zipcode
